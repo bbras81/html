5d48545233fcc2788453177615fd4b99c61472f9
Bruno Miguel Brás <miguelazevedodg@gmail.com>
2024-03-06 15:07:26 +0000

    Ultimos retoques da função listar

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,63 +1,67 @@
 const reservas = [
-    {
-        nome: 'Bruno Brás 1',
-        telemovel: '912813460',
-        email: 'bmabras@gmail.com',
-        hora: '20:30',
-        nrPessoas: 2
-    },
-    {
-        nome: 'Bruno Brás 2',
-        telemovel: '912813460',
-        email: 'bmabras@gmail.com',
-        hora: '20:30',
-        nrPessoas: 2
-
-    },
-    {
-        nome: 'Bruno Brás 3',
-        telemovel: '912813460',
-        email: 'bmabras@gmail.com',
-        hora: '20:30',
-        nrPessoas: 2
-
-    },
-    {
-        nome: 'Bruno Brás 4',
-        telemovel: '912813460',
-        email: 'bmabras@gmail.com',
-        hora: '20:30',
-        nrPessoas: 2
-
-    },
-]
+  {
+    nome: "Bruno Brás 1",
+    telemovel: "912813460",
+    email: "bmabras@gmail.com",
+    hora: "20:30",
+    nrPessoas: 2,
+  },
+  {
+    nome: "Bruno Brás 2",
+    telemovel: "912813460",
+    email: "bmabras@gmail.com",
+    hora: "20:30",
+    nrPessoas: 2,
+  },
+  {
+    nome: "Bruno Brás 3",
+    telemovel: "912813460",
+    email: "bmabras@gmail.com",
+    hora: "20:30",
+    nrPessoas: 2,
+  },
+  {
+    nome: "Bruno Brás 4",
+    telemovel: "912813460",
+    email: "bmabras@gmail.com",
+    hora: "20:30",
+    nrPessoas: 2,
+  },
+  {
+    nome: "Bruno Brás 5",
+    telemovel: "912813460",
+    email: "bmabras@gmail.com",
+    hora: "20:30",
+    nrPessoas: 2,
+  },
+];
 
 function listar(dados) {
-    // Recebe o pointer para o container onde colocar os dados
-    const container = document.querySelector('#lista')
+  // Recebe o pointer para o container onde colocar os dados
+  const container = document.querySelector("#lista");
 
+  // Variavel de buffer, que recebe as tags html para as colocar no container
+  let buffer = [];
+  var str = "";
 
-    // Variavel de buffer, que recebe as tags html para as colocar no container
-    let buffer = []
-
-
-    for (i = 0; i < dados.length; i++) {
-        var str =`
-            < a href = "#" class="list-group-item list-group-item-action " aria - current="true" >
+  for (i = 0; i < dados.length; i++) {
+    str = `
+        <a href="#" class="list-group-item list-group-item-action " aria-current="true">
         <div class="d-flex w-100 justify-content-between">
           <h5 class="mb-1">${dados[i].nome}</h5>
-          <small>${dados[i].nome}</small>
+          <small>${dados[i].hora}</small>
         </div>
         <div class="d-flex w-100 justify-content-between">
           <p class="mb-1">email</p>
-          <small>Nr Pessoas</small>
+          <small>${dados[i].email}</small>
         </div>
-        <small>Telemovel</small>
-      </a > `
-        console.log(dados[i].nome)
+        <small>${dados[i].telemovel}</small>
+      </a> `;
+    buffer += str;
+    container.innerHTML = buffer;
 
-    }
-
+    console.log();
+  }
 }
 
-listar(reservas)
+listar(reservas);
